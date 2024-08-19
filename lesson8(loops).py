14bdfb5c0a22e2f076c2dff5da2318b82dd71760
Oluwanifemi Ojewusi <166236464+Peaceyy28@users.noreply.github.com>
2024-08-19 14:16:49 +0100

    Add files via upload

---
+++ lesson8(loops).py
@@ -0,0 +1,50 @@
+# value = 1
+# while value <= 10:
+#     print(value)
+#     if value ==5:
+#         break
+#     value += 1
+
+# value = 1 
+# while value <= 10:
+#     value += 1
+#     if value ==5:
+#         continue
+#     print(value)
+# else:
+#     print("value is now equal to" + str(value))
+    
+Names = ["nife","peace", "joy"]
+# for x in Names:
+#     print(x)
+
+# for x in "Mississippi":
+#     print(x)
+    
+# for x in Names:
+#     if x == "peace":
+#         break
+#     print(x)
+
+# for x in Names:
+#     if x == "peace":
+#         continue
+#     print(x)
+
+# for x in range(4):
+#     print(x)
+    
+# # for x in range(2, 4):
+# print(x)
+
+# for x in range(5, 101, 5):
+#     print(x)
+# else:
+#     print("Glad that\'s over!")
+
+Names = ["nife","peace", "joy"]
+actions = ["codes", "eats", "sleeps"]
+
+for name in Names:
+    for action in action:
+        print(name + " " + action ".")
